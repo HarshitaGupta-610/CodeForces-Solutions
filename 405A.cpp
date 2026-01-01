89b10c2d500eea7a4f0c960a276bbb3a463156a5
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-01-02 01:00:04 +0530

    Update 405A.cpp

--- 405A.cpp
+++ 405A.cpp
@@ -1,1 +1,34 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    queue<pair<int,int>> q;
+
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        q.push({a, i + 1});   
+    }
+
+    int ans = 0;
+
+    while (!q.empty()) {
+        auto child = q.front();
+        q.pop();
+
+        child.first -= m;     
+        ans = child.second;  
+
+        if (child.first > 0) {
+            q.push(child);   
+        }
+        
+    }
+
+    cout << ans << endl;
+    return 0;
+}
