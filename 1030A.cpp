86b745afe1f9f0e6c60fbe14eb6bdf6a2e89330a
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-01-05 22:48:41 +0530

    Create 1030A.cpp

---
+++ 1030A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int c0 = 0 , c1 = 0;
+    int a;
+    for(int i = 0 ; i < n ; i++){
+        cin >> a ;
+        if(a == 0 ){
+            c0++;
+        }
+        else{
+            c1++;
+        
+    }
+}
+if(c1 == 0){
+    cout<<"EASY";
+}
+else{
+    cout<<"HARD";
+}
+return 0;
+}
