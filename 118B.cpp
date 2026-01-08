fba9dd7d2858b76e6bd4439e2776e71a12d5da76
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-01-09 02:48:08 +0530

    Create 118B.cpp

---
+++ 118B.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i <= n; i++) {
+
+        if (i != n) {
+            for (int j = 1; j < n - i; j++) {
+                cout << "  ";
+            }
+            {
+                cout << " ";
+            }
+        }
+
+        if (i == n) {
+            for (int j = 0; j < i; j++) {
+                cout << j << " ";
+            }
+            {
+                cout << i;
+            }
+        } 
+        else {
+            for (int j = 0; j <= i; j++) {
+                cout << " " << j;
+            }
+        }
+
+        if (i != 0) {
+            for (int j = i - 1; j >= 0; j--) {
+                cout << " " << j;
+            }
+        }
+
+        {
+            cout << "\n";
+        }
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+
+        for (int j = 1; j < n - i; j++) {
+            cout << "  ";
+        }
+
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++) {
+            cout << " " << j;
+        }
+
+        if (i != 0) {
+            for (int j = i - 1; j >= 0; j--) {
+                cout << " " << j;
+            }
+        }
+
+        {
+            cout << "\n";
+        }
+    }
+
+    return 0;
+}
+
