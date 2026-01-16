7a3f61ef1affa9cd986d34305dc69ca3d1fa557d
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-01-16 19:00:06 +0530

    Create 1703A.cpp

---
+++ 1703A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string to_lower(string s) {
+    for (char c : s){
+        c = tolower(c);
+    }
+    return s;
+}
+
+int main(){
+   int t;
+   cin >> t;
+   while(t--){
+      string s;
+      cin >> s;
+      s = to_lower(s);
+
+      if (s == "yes"){
+         cout << "YES\n";
+      }
+      else{
+         cout << "NO\n";
+      }
+   }
+}
