8257a3d72c2611b7bb8120d0db43529d8d0cdd79
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-03-17 23:25:31 +0530

    Implement check function for sum validation

---
+++ 1878C.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void check(long long n, long long k, long long x){
+    long long min_sum = (k * (k + 1)) / 2;
+    long long max_sum = (k * (2*n - k + 1)) / 2;
+
+    if(x >= min_sum && x <= max_sum){
+        cout << "YES\n";
+    }
+    else{
+        cout << "NO\n";
+    }
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long n, k, x;
+        cin >> n >> k >> x;
+        check(n, k, x);
+    }
+}
