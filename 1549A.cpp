16a9139fbe003413f3bd6c77d92c0adc390d9ecb
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-06-06 22:40:56 +0530

    Add solution for problem 1549A

---
+++ 1549A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>> t;
+    while(t--){
+        int P;
+        cin >>P;
+        cout<<2<<" "<<P-1<<endl;
+    }
+    return 0;
+}
