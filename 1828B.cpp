13b37b76670d6ec968662990d66e63bb6da837ca
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-03 22:39:15 +0530

    Create 1828B.cpp

---
+++ 1828B.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int>a(n+1);
+        for(int i = 1 ; i <= n ; i++){
+            cin >> a[i];
+        }
+        int ans = 0;
+        for(int i = 1 ; i <= n ; i++){
+            int dis = abs(a[i] - i);
+            ans = gcd(ans , dis);
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
