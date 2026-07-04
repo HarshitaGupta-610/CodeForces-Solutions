5edd9547554f766d0cac4e4ef4ada317bd2a2d5c
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-04 19:05:15 +0530

    Create 1537B.cpp

---
+++ 1537B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long n, m, i, j;
+    cin >> n >> m >> i >> j;
+    cout << 1 << " " << 1 << " " << n << " " << m << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
