7f5190dcd4f5304ce368fa29b7504d6828f3347f
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-09-08 16:04:45 +0530

    Create 2209B.cpp

---
+++ 2209B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int t;
+cin >> t;
+while (t--) {
+ int n;
+cin >> n;
+vector<long long> a(n);
+for (int i = 0; i < n; i++) {
+cin >> a[i];
+ }
+for (int i = 0; i < n; i++) {
+int greater = 0;
+int smaller = 0;
+for (int j = i + 1; j < n; j++) {
+if (a[j] > a[i]) greater++;
+else if (a[j] < a[i]) smaller++;
+}
+ cout << max(greater, smaller) << " ";
+}
+cout << endl;
+    }
+    return 0;
+}
