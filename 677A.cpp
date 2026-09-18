fe0be47e07764de6eb941f4024379925026ac95f
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-09-18 15:02:37 +0530

    Implement height counting logic in 677A.cpp

---
+++ 677A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n , h;
+cin >> n >> h;
+int count = 0;
+vector<int>a(n);
+for(int i = 0 ; i < n ; i++){
+cin >> a[i];
+if(a[i] > h) count += 2;
+else count += 1;
+}
+cout<<count<<endl;
+return 0;
+}
