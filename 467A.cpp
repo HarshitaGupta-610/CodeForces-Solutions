e571f716a331f2a32922069ae4e0d7f76f543a7e
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-09-19 22:15:22 +0530

    Create 467A.cpp

---
+++ 467A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n ;
+  cin >> n;
+int rooms = 0;
+  while(n--){
+    int p , q;
+    cin >> p >> q;
+    if(q-p >= 2) rooms++;
+  }
+  cout<<rooms<<endl;
+  return 0;
+}
