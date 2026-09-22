729a72f2b5598643d01de24de67a95c683517bd9
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-09-22 20:23:55 +0530

    Update 510A.cpp

--- 510A.cpp
+++ 510A.cpp
@@ -1,33 +1,36 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n, m;
-    cin >> n >> m;
+int main(){
+int n,m;
+cin>>n>>m;
 
-    int evenCount = 0;
+bool left=false;
+bool right=true;
 
-    for (int i = 1; i <= n; i++) {
+for(int i=1;i<=n;i++){
 
-        if (i % 2 == 1) {
-            for (int j = 0; j < m; j++)
-                cout << "#";
-        } 
-        else {
-            evenCount++;
-
-            if (evenCount % 2 == 1) {
-                for (int j = 0; j < m - 1; j++)
-                    cout << ".";
-                cout << "#";
-            } 
-            else {
-                cout << "#";
-                for (int j = 0; j < m - 1; j++)
-                    cout << ".";
-            }
+    if(i%2!=0){
+        for(int j=0;j<m;j++)
+            cout<<"#";
+    }
+    else{
+        if(right){
+            for(int j=0;j<m-1;j++)
+                cout<<".";
+            cout<<"#";
+        }
+        else{
+            cout<<"#";
+            for(int j=0;j<m-1;j++)
+                cout<<".";
         }
 
-        cout << "\n";
+        swap(left,right);
     }
+
+    cout<<endl;
+}
+
+return 0;
 }
